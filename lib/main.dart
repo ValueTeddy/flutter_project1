5129364728248b18e0f04af62dc6e80a9b6db980
Teddy <teodor.adolfsson@gmail.com>
2022-06-19 04:11:36 +0200

    good message

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,27 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _questions = [
     {
-      'questionText':
-          'Question 1 AA\n\n\AA\n\n\AA\n\n\AA\n\n\AAA\n\n\AAAAAAAAAAAAAAAAAAAAA',
-      'answers': ['abc', '123', 'wow!'],
+      'q': 'The economy is: ',
+      'ans1': 'Strong',
+      'ans2': 'Weak',
     },
     {
-      'questionText': 'Question 2',
-      'answers': ['abc', '123', 'yikes!'],
+      'q': 'The economic outlook is: ',
+      'ans1': 'Positive',
+      'ans2': 'Negative',
     },
     {
-      'questionText': 'Question 3',
-      'answers': ['abc', '123', 'hey!'],
+      'q': 'Lenders are:  ',
+      'ans1': 'Eager',
+      'ans2': 'Reticent',
     },
   ];
-
+  double _fontsize = 20;
   var _questionIndex = 0;
 
   void _answerQuestion() {
-    setState(() {
-      _questionIndex += 1;
-    });
+    setState(() =>_questionIndex += 1);
+    //String ans =
   }
 
   int getQuestionIndex() {
@@ -46,37 +47,105 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  Widget quiz(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
+  Widget q0(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Container(
+        Expanded(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(_questions[0]['questionText'].toString(),
-                  style: TextStyle(fontSize: 28)),
-              ElevatedButton(onPressed: _answerQuestion, child: Text('button')),
+              Text(_questions[0]['q'].toString(),
+                  style: TextStyle(fontSize: _fontsize)),
             ],
           ),
         ),
-        Container(
+        Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              ElevatedButton(
+                onPressed: _answerQuestion,
+                child: Text(_questions[0]['ans1'].toString()),
+              ),
+              ElevatedButton(
+                onPressed: _answerQuestion,
+                child: Text(_questions[0]['ans2'].toString()),
+              ),
+            ]),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget q1(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(_questions[1]['questionText'].toString(),
-                  style: TextStyle(fontSize: 28)),
-              ElevatedButton(onPressed: _answerQuestion, child: Text('button')),
+              Text(_questions[1]['q'].toString(),
+                  style: TextStyle(fontSize: _fontsize)),
             ],
           ),
         ),
-        Container(
+        Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              ElevatedButton(
+                onPressed: _answerQuestion,
+                child: Text(_questions[1]['ans1'].toString()),
+              ),
+              ElevatedButton(
+                onPressed: _answerQuestion,
+                child: Text(_questions[1]['ans2'].toString()),
+              ),
+            ]),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget q2(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(_questions[2]['questionText'].toString(),
-                  style: TextStyle(fontSize: 28)),
-              ElevatedButton(onPressed: _answerQuestion, child: Text('button')),
+              Text(_questions[2]['q'].toString(),
+                  style: TextStyle(fontSize: _fontsize)),
             ],
           ),
         ),
+        Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              ElevatedButton(
+                onPressed: _answerQuestion,
+                child: Text(_questions[2]['ans1'].toString()),
+              ),
+              ElevatedButton(
+                onPressed: _answerQuestion,
+                child: Text(_questions[2]['ans2'].toString()),
+              ),
+            ]),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget quiz(BuildContext context) {
+    return Column(
+      children: [
+        q0(context),
+        q1(context),
+        q2(context),
       ],
     );
   }
@@ -92,25 +161,18 @@ class _MyAppState extends State<MyApp> {
           body: ListView(
             padding: const EdgeInsets.all(10),
             children: [
-              BackButton(
-                onPressed: null,
-              ),
               _questionIndex < _questions.length
                   ? Column(
                       //quiz
                       children: [quiz(context)],
                     )
                   : Result(), // end page
-              BottomAppBar(
-                child: Text("this is the bottom"),
-              )
             ],
           ),
           bottomNavigationBar: BottomAppBar(
             child: OverflowBar(
               children: [
                 TextButton(onPressed: _reset, child: const Text("reset")),
-                const Text("this is a great app"),
               ],
             ),
           ),
