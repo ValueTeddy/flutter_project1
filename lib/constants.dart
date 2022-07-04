3ae1903710461c8b3cc0a10575677692e72f4ad7
Teddy <teodor.adolfsson@gmail.com>
2022-07-04 16:54:51 +0200

    asd

---
+++ lib/constants.dart
@@ -0,0 +1,26 @@
+
+
+double fontsize = 20;
+
+final questions = [
+  {
+    'q': 'The economy is: ',
+    'ans1': 'Strong',
+    'ans2': 'Weak',
+  },
+  {
+    'q': 'The economic outlook is: ',
+    'ans1': 'Positive',
+    'ans2': 'Negative',
+  },
+  {
+    'q': 'Lenders are:  ',
+    'ans1': 'Eager',
+    'ans2': 'Reticent',
+  },
+  {
+    'q': 'Capital markets are:  ',
+    'ans1': 'Loose',
+    'ans2': 'Tight',
+  },
+];
---
+++ lib/quiz.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import './result.dart';
+import './constants.dart';
+import './quiz.dart';
+import './main.dart';
+
